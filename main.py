12774cfecb9a1f897d8df3c22c7132c0ea2596c1
Gabriel Tessarolo Helmer <gabriel.tessarolo.h@gmail.com>
2022-12-23 11:46:13 -0300

    commit inicial

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pyautogui as pag
 import random
 
-#names = ['Gabriel', 'Guilherme', 'Átila', 'Laura', 'Amanda']
-#emails = ['tessarolohelmer@gmail.com', 'lanierguilherme@gmail.com', 'atilassooll@gmail.com', 'laura.oliveira.carvalho82@gmail.com', '06amandareis@gmail.com']
+#This code is an auto-emailing for secret friend game. It works using the PyAutoGUI library, and the screen resolution is 1920*1080px.
 
-dic1 = {'tessarolohelmer@gmail.com': 'Gabriel',
-        'lanierguilherme@gmail.com': 'Guilherme',
-        'atilassooll@gmail.com': 'Atila',
-        'laura.oliveira.carvalho82@gmail.com': 'Laura',
-        '06amandareis@gmail.com': 'Amanda'}
+dic1 = {}
+
+numPlayers = int(input('\033[1;97mSECRET FRIEND | \033[1;31mHow many friends will play? '))
+
+for i in range(0, numPlayers):
+    dic1[input(f'\033[1;92mFriend {i+1} \033[mEmail: ')] = input(f'\033[1;92mFriend {i+1} \033[mName: ')
+    print('\033[1;97m'+('-'*20));
+
+mainEmail = input('\033[1;97mSECRET FRIEND | \033[1;31mWhat email will send the sorted names? ') #Here, paste the link of the main email's mail box.
 
 pag.sleep(1)
 
@@ -17,7 +20,7 @@ pag.click(x=954, y=1060)
 def sendEmail(email, name):
     pag.sleep(1)
     pag.hotkey('ctrl','t')
-    pag.write('https://mail.google.com/mail/u/1/?ogbl#inbox')
+    pag.write(mainEmail)
     pag.press('enter')
     pag.sleep(3)
     pag.click(x=117, y=204)
@@ -35,24 +38,20 @@ def sendEmail(email, name):
 emails = [i for i in dic1.keys()]
 names = [i for i in dic1.values()]
 
-#for c, i in enumerate(names,1):
-for i in range(0,5):
-    actualEmail = random.choice(emails)
-    newList = [x for x in names if dic1[actualEmail] != x]
-    actualName = random.choice(newList)
+for i in range(0, len(emails)):
+    actualEmail = random.choice(emails) #sort an email
+    newList = [x for x in names if dic1[actualEmail] != x] #create a list with containing other people
+    actualName = random.choice(newList) #sort a name in this list
 
-    sendEmail(actualEmail, actualName)
+    sendEmail(actualEmail, actualName) #send the email to the sorted person
 
-    emails.remove(actualEmail)
-    names.remove(actualName)
+    emails.remove(actualEmail) #remove the sorted email from the emails list
+    names.remove(actualName) #remove the sorted name from the names list
 
-    #sortear um email
-    #criar uma lista com todos os nomes diferentes do email
-    #sortear algo nessa lista
-    #
+    #this way, nobody is gonna draw itself, and two or more people won't draw the same person.
 
 pag.sleep(5)
-pag.hotkey('alt','f4')
+pag.hotkey('alt','f4') #close the email box, so you won't see what's have been sorted!
 
 
 
